3d2ad0ec68d7e16fcc61ff4cbd8cf0de0121b918
Filipe Sales <devfilipesales@gmail.com>
2020-01-02 19:36:05 -0300

    ajustes e crianção do campo

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 window.onload = function() {
 
     var stage = document.getElementById('stage');
-    var ctx = stage.getContext('2d');
+    var ctx = stage.getContext("2d");
+
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, stage.clientWidth, stage.height);
 
 }
