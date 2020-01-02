2b7316cd5918cce509274dd96c045572d5bdfa47
Filipe Sales <devfilipesales@gmail.com>
2020-01-02 19:40:51 -0300

    velocidade

--- script.js
+++ script.js
@@ -3,7 +3,19 @@ window.onload = function() {
     var stage = document.getElementById('stage');
     var ctx = stage.getContext("2d");
 
-    ctx.fillStyle = "black";
-    ctx.fillRect(0, 0, stage.clientWidth, stage.height);
+    setInterval(game, 60);
+
+    const vel = 1;
+
+    var vx = vy = 0;
+    var px = 10;
+    var py = 15;
+
+    function game() {
+
+        ctx.fillStyle = "black";
+        ctx.fillRect(0, 0, stage.clientWidth, stage.height);
+
+    }
 
 }
